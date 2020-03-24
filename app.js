a69fb29af54a45be4c01a628b6009c872df3acb0
HectorBerry <hectorberryg@gmail.com>
2020-03-24 23:17:57 +0100

    FIX: CORS bug

--- app.js
+++ app.js
@@ -3,14 +3,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 // CONST
-const PORT = 4400;
+const PORT = 3300;
 const PHONES = require('./phones.json');
 
 // Import express into const app
 const app = express();
 
+// MIDDLEWARE
+
 // Apply json body parser
 app.use(bodyParser.json());
+
+// Add headers to let react app connect
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 
 // Routes
 app.get('/phones', (req, res) => {
